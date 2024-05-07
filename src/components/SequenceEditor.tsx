b48c527df0a4b68b291ae09d02c9075b5ce983bf
obisopulu <obisopulu@gmail.com>
2024-05-07 22:06:31 +0200

    chore: Refactor SequenceEditor component and update input fields styling

--- src/components/SequenceEditor.tsx
+++ src/components/SequenceEditor.tsx
@@ -4,7 +4,7 @@
 import MachineCard from "@/components/MachineCard";
 import { useState } from "react";
 
-export default function SequenceEditor(machineData) {
+export default function SequenceEditor(machineData: string | number) {
   const [components, setComponents] = useState<JSX.Element[]>([]);
   let key = components.length;
 
@@ -17,22 +17,27 @@ export default function SequenceEditor(machineData) {
   };
 
   return (
-    <div className='bg-slate-600 flex flex-1 justify-center items-center p-12 h-[100vh] overflow-auto'>
+    <div className='bg-slate-600 flex flex-1 justify-center items-center'>
       
       <div className="p-4 bg-slate-700 w-[840px]">
-        <div className="text-white pb-2">Machine State:</div>
+        <div className=" pb-2">Machine State:</div>
           
-        <div className="flex flex-row text-4xl text-white gap-4">
+        <div className="flex flex-row text-4xl  gap-4">
           RecipeName: 
-          <input onChange={handleChange} name="RecipeName" className="block mb-4 text-white text-4xl bg-slate-700 w-full" />
+          <input onChange={handleChange} name="RecipeName" className="block mb-4 ml-4  text-4xl bg-slate-600 w-full" />
         </div>
         
-        <div className="flex flex-row text-2xl text-white">
-          SequenceA: 
-          <input onChange={handleChange} name="RecipeName" className="block mb-4 text-white bg-slate-700 w-full" />
+        <div className="flex flex-row text-2xl ">
+          StartSequenceId: 
+          <input onChange={handleChange} name="RecipeName" className="block mb-4 ml-4  bg-slate-600 w-full" />
         </div>
         
-        <div className="flex flex-row text-2xl text-white mb-2">
+        <div className="flex flex-row text-xl ">
+          StartStepId: 
+          <input onChange={handleChange} name="RecipeName" className="block mb-4 ml-4  bg-slate-600 w-full" />
+        </div>
+        
+        <div className="flex flex-row text-2xl  mb-2">
           Steps:
         </div>
 
@@ -42,7 +47,7 @@ export default function SequenceEditor(machineData) {
           ))}
         </div>
         
-        <button className="p-4 mt-4 bg-slate-600 text-white" onClick={addComponent}>Add Step</button>
+        <button className="p-4 mt-4 bg-slate-600 " onClick={addComponent}>Add Step</button>
       </div>
     </div>
   );
