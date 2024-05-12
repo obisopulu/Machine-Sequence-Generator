539d1d525fb58591bef09c26a7663daca275551d
obisopulu <obisopulu@gmail.com>
2024-05-12 19:36:40 +0200

    Refactor FileTransfer hook and improve file handling

--- src/hooks/FileTransfer.tsx
+++ src/hooks/FileTransfer.tsx
@@ -7,16 +7,18 @@ type fetchDataProps = {
   data?: Record<string, any>;
 };
 
-export const transferData = async ({folder, fileName, data}: fetchDataProps) => {
+export const transferData = async ({ folder, fileName, data }: fetchDataProps) => {
   try {
     if (data) {
       const jsonData = JSON.stringify(data, null, 2);
-      fs.writeFile('src/Task/data.json', jsonData);
-      return('Data has been written to data.json');
-    }else{
-      const filePath = path.join(process.cwd(), 'src', folder, fileName);
-      const jsonData = JSON.parse(await fs.readFile(filePath, 'utf-8'));
-      return jsonData;
+      await fs.writeFile(path.join(process.cwd(), 'public', 'data.json'), jsonData);
+      return 'Data has been written to data.json';
+    } else {
+      if (typeof folder === 'string' && typeof fileName === 'string') {
+        const filePath = path.join(process.cwd(), 'src', folder, fileName);
+        const jsonData = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+        return jsonData;
+      }
     }
   } catch (error) {
     console.error("Error reading file:", error);
