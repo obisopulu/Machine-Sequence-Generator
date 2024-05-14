0c799e007abe1b029a9afa61bded6d7d916e8059
obisopulu <obisopulu@gmail.com>
2024-05-14 16:57:31 +0200

    Refactor page.tsx to remove file transfer functionality and update SequenceEditor component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import SequenceEditor from "@/components/SequenceEditor";
-import { transferData } from "@/hooks/FileTransfer";
 
-export default async function Home() {
-  const data = await transferData({ folder: 'Task', fileName: 'MachineCapabilities_Assessment.json' });
-  
-  transferData({ data: data})
+export default function Home() {
+
 
   return (
     <div className='flex justify-center text-white py-12'>
-        <SequenceEditor data={data} />
+        <SequenceEditor />
     </div>
   );
 }
